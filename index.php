c44235fe969bdcc2e2c2a0b7aa8efbe5fd5ca520
BrookSaranyapong <117848876+BrookSaranyapong@users.noreply.github.com>
2023-07-10 00:27:15 +0700

    first commit

--- index.php
+++ index.php
@@ -5,15 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <?php include('./includes/theme.php') ?>
+    <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css" class="rel">
 </head>
 
 <body>
     <?php include('./includes/navbar.php'); ?>
 
     <h1>Home Page</h1>
-    <?php include('./includes/script.php') ?>
  
+    <script src="./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
 
 </html>
