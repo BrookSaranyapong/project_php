e9d7c30576ba3f93303391c13e33689cf3053d16
Supakorn Navamavad <truemoneythailand@gmail.com>
2023-07-18 23:14:44 +0700

    update delete

--- page/admin/pages/admin/delete.php
+++ page/admin/pages/admin/delete.php
@@ -3,11 +3,20 @@
 
     $id = $_GET['id'];
     if (isset($id) && $id != 1) {
-        $sql = "DELETE FROM `user` WHERE `admin`.`id` = ? ";
+        $sqlprofile = "SELECT image FROM `user` WHERE u_id = :id ";
+        $stmt = $conn->prepare($sqlprofile);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        $rowprofile = $stmt->fetch(PDO::FETCH_ASSOC);
+        unlink('../../../../assets/image/imageMember/'.$rowprofile['image']);
+
+        $sql = "DELETE FROM `user` WHERE u_id = :id ";
         $result = $conn->prepare($sql);
-        $result->execute([$id]);
+        $result->bindParam(':id', $id);
+        $result->execute();
         
-        if($conn->$result->rowCount() > 0){
+        //if($conn->$result->rowCount() > 0){
+        if($result->execute()){
             echo '<script> alert("Finished Deleting!")</script>'; 
             header('Refresh:0; url=index.php');
         } else {
